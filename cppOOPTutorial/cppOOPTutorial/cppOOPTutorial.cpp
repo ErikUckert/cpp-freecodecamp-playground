c05e7d342188a778f9d1a07c256ccbfc849812ce
Erik Uckert <uckert@outlook.com>
2021-02-15 11:17:30 +0100

    add the vs project with first cpp src code

---
+++ cppOOPTutorial/cppOOPTutorial/cppOOPTutorial.cpp
@@ -0,0 +1,53 @@
+// cppOOPTutorial.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include <string>
+
+
+
+// creating a class
+class Employee {
+	public: // access moidifier --> private / protected / public
+		std::string Name;
+		std::string Company;
+		int Age;
+
+		void IntroduceYourself() {
+			std::cout << "My Name is " << Name << std::endl;
+			std::cout << "I work for " << Company << std::endl;
+			std::cout << "I'm age " << Age << std::endl;
+		}
+};
+
+int main()
+{
+
+	// constructing two objects of the employee class by hand
+	
+	// Object 1 "Erik"
+    std::cout << "Hello World!\n";
+	Employee employee1;
+	employee1.Name = "Erik";
+	employee1.Company = "AVL";
+	employee1.Age = 31;
+	employee1.IntroduceYourself();
+
+	// Object 2 "John"
+	Employee employee2;
+	employee2.Name = "John";
+	employee2.Company = "Amazon";
+	employee2.Age = 33;
+	employee2.IntroduceYourself();
+}
+
+// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
